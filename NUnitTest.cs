fbd30c3ef045f0dc3af562e3bf35cf4f34b033f5
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-11-13 14:08:59 +0200

    Intial commit
    Added Project files from the 6.2 - [Selenium] exercise, aka the exercise where we setup Selenium for the project

---
+++ NUnitTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumC_
+{
+    class NUnitTest
+    {
+        public ChromeDriver driver;
+
+
+        [SetUp]
+        public void Initialize()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void OpenappTest()
+        {
+            driver.Url = ("https://Google.com");
+        }
+
+        [TearDown]
+        public void EndTest()
+        {
+            driver.Quit();
+        }
+    }
+}
+
+
